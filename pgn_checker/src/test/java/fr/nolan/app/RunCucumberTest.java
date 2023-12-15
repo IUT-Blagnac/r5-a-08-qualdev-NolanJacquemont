b5848ad63e5b562122c0977edd792e03063097d8
Nolan <nolan.jacquemont@gmail.com>
2023-12-15 18:41:36 +0100

    Added cukedoctor to PgnChecker tp4

--- pgn_checker/src/test/java/fr/nolan/app/RunCucumberTest.java
+++ pgn_checker/src/test/java/fr/nolan/app/RunCucumberTest.java
@@ -1,9 +1,11 @@
 package fr.nolan.app;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"json:target/cucumber.json"} )
 public class RunCucumberTest {
 
 }
